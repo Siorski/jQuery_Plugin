fd072449a4e22bd2a908fdd0ca6eba6e1ea83540
Siorski <kacpersiora@wp.pl>
2014-05-22 16:14:05 +0200

    sila hasla

--- js/plugin.js
+++ js/plugin.js
@@ -52,7 +52,29 @@ var jQuery;
 				}
 			},
 			walidacjaHasla: function (pole){
-				var haslo = $(pole).val();		
+				var haslo = $(pole).val();						
+				var wielkieLitery = /[A-Z]/g; // wyszukiwanie wielkich liter w haśle
+				var maleLitery = /[a-z]/g;		// wyszukiwanie małych liter w haśle
+				var cyfry = /[0-9]/g;					// wyszukiwanie cyfr w haśle
+				var znakiSpecjalne = /[!,@,#,$,%,^,&,*,?,_,~]/g; //wyszukiwanie znaków specjalnych w haśle
+				var silaHasla = 0;
+				
+				var zliczWystapienia = function (string){
+					var liczbaZnakow = haslo.match(string);
+					if (liczbaZnakow === null){
+						liczbaZnakow = 0;
+					}
+					else{
+						liczbaZnakow = haslo.match(string).length;
+					}
+					return liczbaZnakow;
+				};
+
+				var wielkieLiteryIlosc = zliczWystapienia(wielkieLitery);
+				var maleLiteryIlosc = zliczWystapienia(maleLitery);
+				var cyfryIlosc = zliczWystapienia(cyfry);
+				var znakiSpecjalneIlosc = zliczWystapienia(znakiSpecjalne);
+
 				// sprawdzanie długości hasla
 				if (haslo.length < ustawienia.minDlugoscHasla){
 					funkcje.blad(pole);
@@ -60,6 +82,61 @@ var jQuery;
 				else{	
 					funkcje.sukces(pole);
 				}
+				// litery w haśle
+				if (wielkieLiteryIlosc === 0 && maleLiteryIlosc !== 0){ 
+					silaHasla = silaHasla + 3; 
+				}
+				if (wielkieLiteryIlosc !== 0 && maleLiteryIlosc ===0){
+					silaHasla = silaHasla + 5;
+				}
+				if (wielkieLiteryIlosc !== 0 && maleLiteryIlosc !== 0){ 
+					silaHasla = silaHasla + 10; 
+				}
+				// cyfry w haśle
+				if (cyfryIlosc === 1){
+					silaHasla = silaHasla + 3;
+				}
+				if (cyfryIlosc === 2){
+					silaHasla = silaHasla + 5;
+				}
+				if (cyfryIlosc > 2){
+					silaHasla = silaHasla + 10;
+				}
+				//znaki specjalne w haśle
+				if (znakiSpecjalneIlosc === 1){
+					silaHasla = silaHasla + 3;
+				}   
+				if (znakiSpecjalneIlosc === 2){
+					silaHasla = silaHasla + 5;
+				}
+				if (znakiSpecjalneIlosc > 2){
+					silaHasla = silaHasla + 10;
+				}
+				// długość hasła
+				if (haslo.length < 6){
+					silaHasla = silaHasla + 1;
+				}
+				if ((haslo.length > 6) && (haslo.length <= 8) ){
+					silaHasla = silaHasla + 2;
+				}
+				if (haslo.length > 8){
+					silaHasla = silaHasla + 3;
+				}
+				if (haslo.length > 15){
+					silaHasla = silaHasla + 10;
+				}
+				var silaHaslaText = "";
+				if (silaHasla >= 20){
+					silaHaslaText = "Hasło silne";
+				}
+				else if (silaHasla >= 10){
+					silaHaslaText = "Hasło średnie";
+				}
+				else{
+					silaHaslaText = "Hasło słabe";
+				}
+				$("#silaHasla").remove();
+				$(pole).parent().append("<p id='silaHasla'>" + silaHaslaText + "</p>");
 			},
 			blokadaGuzika: function(){
 				var elementyInput = $(formularzGlobal).find('input');
@@ -69,8 +146,7 @@ var jQuery;
 						formularzWypelnionyPrawidlowo = 0;
 					}
 				}
-				
-				if( formularzWypelnionyPrawidlowo === 0) {
+				if (formularzWypelnionyPrawidlowo === 0) {
 					$($(formularzGlobal).find('[id="wyslijBTN"]')).attr('disabled', true);
 				}
 				else {
@@ -86,7 +162,7 @@ var jQuery;
 			},
 			sukces: function (pole) {
 				if($(pole).hasClass('walidacjaNiepoprawna')) {
-					$(pole).removeClass('walidacjaNiepoprawna');
+							$(pole).removeClass('walidacjaNiepoprawna');
 				}
 				$(pole).addClass('walidacjaPoprawna');
 				funkcje.blokadaGuzika();
